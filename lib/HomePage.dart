3820227f331dde190fba47eaf2eb98a775c2387d
Shubham-Pandya16 <user.shubham16@gmail.com>
2024-08-24 23:15:28 +0530

    Creating Sample Post Structure in HomePage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
@@ -140,6 +141,7 @@ class _HomePageState extends State<HomePage> {
                                 margin: EdgeInsets.only(bottom: 10, right: 15),
                                 child: Column(
                                   children: [
+                                    // Story Profile Picture Outline
                                     ClipOval(
                                       child: Container(
                                         padding: EdgeInsets.all(2),
@@ -155,6 +157,7 @@ class _HomePageState extends State<HomePage> {
                                             end: Alignment.bottomRight,
                                           ),
                                         ),
+                                        // Story Profile Picture
                                         child: ClipOval(
                                           child: Image.asset(
                                             "assets/images/pfp/ana_pfp.jpg",
@@ -165,6 +168,7 @@ class _HomePageState extends State<HomePage> {
                                         ),
                                       ),
                                     ),
+                                    // Story Username
                                     Text(
                                       "ana_p_1206",
                                       style: TextStyle(
@@ -181,7 +185,137 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
-                  Text("posts"),
+                  // Sample Post
+                  Card(
+                    child: Container(
+                      padding: EdgeInsets.all(sHeight * 0.015),
+                      width: sWidth * 0.93,
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(30, 30, 30, 1.0),
+                          borderRadius: BorderRadius.circular(25)),
+                      child: Column(
+                        children: [
+                          // Post Header
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: sWidth * 0.01,
+                                  vertical: sHeight * 0.003,
+                                ),
+                                // Post Profile Picture and Username
+                                child: Row(
+                                  children: [
+                                    // Post Profile Picture
+                                    ClipOval(
+                                        child: Image.asset(
+                                      "assets/images/pfp/ana_pfp.jpg",
+                                      height: sHeight * 0.05,
+                                    )),
+                                    SizedBox(
+                                      width: sWidth * 0.03,
+                                    ),
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        // Post Name of User
+                                        Text(
+                                          " Ãna Patel",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w700,
+                                              letterSpacing: 0.7,
+                                              fontSize: sHeight * 0.018),
+                                        ),
+                                        // Post Username
+                                        Text(
+                                          " ana_p_1206",
+                                          style: TextStyle(
+                                              fontSize: sHeight * 0.015,
+                                              fontWeight: FontWeight.w300,
+                                              height: 0.9,
+                                              color: Colors.grey.shade400),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // Post More Icon
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: sWidth * 0.015),
+                                child: Icon(
+                                  Icons.more_vert_rounded,
+                                  color: Colors.grey,
+                                  size: iconSize * 1.2,
+                                ),
+                              ),
+                            ],
+                          ),
+                          // Post Image
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: sHeight * 0.01,
+                            ),
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                    "assets/images/pfp/profile_picture.jpg")),
+                          ),
+                          // Post Action Buttons
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  // Post Like Button
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(left: sWidth * 0.02),
+                                    child: ShaderMask(
+                                      shaderCallback: (Rect bounds) {
+                                        return LinearGradient(
+                                                colors: [
+                                              Colors.red,
+                                              Colors.purple,
+                                              Colors.pink,
+                                              Colors.yellow,
+                                            ],
+                                                begin: Alignment.topLeft,
+                                                end: Alignment.bottomRight)
+                                            .createShader(bounds);
+                                      },
+                                      child: Icon(
+                                        Icons.favorite,
+                                        size: iconSize * 1.4,
+                                      ),
+                                    ),
+                                  ),
+                                  // Post Comment Button
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(left: sWidth * 0.02),
+                                    child: SvgPicture.asset(
+                                      "assets/images/comment.svg",
+                                      color: Colors.grey.shade500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: sWidth * 0.02),
+                                child: Icon(Icons.send_outlined, )
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                   SizedBox(
                     height: sHeight * 0.095,
                   ),
